58076339623ff86daa43b24cad0adf90d1674444
ayoub1brm <105363414+ayoub1brm@users.noreply.github.com>
2024-07-12 17:37:27 +0200

    Update channel_activity_line.py

--- visualizations/channel_activity_line.py
+++ visualizations/channel_activity_line.py
@@ -16,6 +16,7 @@ def channel_activity_line_chart(db,start_date, end_date, granularity):
         fig.add_trace(go.Scatter(
             x=df_pivot.index,
             y=df_pivot[channel_name],
+            mode='lines',
             fill='tozeroy',  # Fill the area under the curve
             name=f'{channel_name}'
         ))
