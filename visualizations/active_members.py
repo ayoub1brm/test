e40a340ceebc13b5c535ec6468b6bab9d47cc988
ayoub1brm <105363414+ayoub1brm@users.noreply.github.com>
2024-07-15 01:53:17 +0200

    Update active_members.py

--- visualizations/active_members.py
+++ visualizations/active_members.py
@@ -1,8 +1,6 @@
 import streamlit as st
 from database_processing.data_processing import active_members
 
-def active_members_chart(db,start_date, end_date, role = False):
-    active_members_count = active_members(db,start_date, end_date, role)
-    interval = end_date - start_date
-    diff_ret = active_members(db,start_date-interval,start_date)
-    st.metric(label="Active Members", value=active_members_count,delta=active_members_count-diff_ret)
+def active_members_chart(db, role = False):
+    active_members_count = active_members(db, role)
+    st.metric(label="Active Members", value=active_members_count)
