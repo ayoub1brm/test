07007c3d4ceac5ab3b54b227dd365bfef5c9ed74
ayoub1brm <105363414+ayoub1brm@users.noreply.github.com>
2024-07-16 16:08:26 +0200

    Update total_members.py

--- visualizations/total_members.py
+++ visualizations/total_members.py
@@ -4,10 +4,4 @@ from database_processing.data_processing import total_members
 
 def total_members_chart(db):
     total_members_count = total_members(db)
-    st.metric(label="Humans", value=total_members_count)
-    # fig = go.Figure(go.Indicator(
-    #     mode = "number",
-    #     value = total_members_count,
-    #     title = {"text": "Total Members"},
-    # ))
-    # st.plotly_chart(fig)
+    st.metric(label="Total", value=total_members_count)
